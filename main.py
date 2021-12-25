b0b90cb86a8158155b5c277af1c2f6c297a496a6
Graham <jonas.stirnemann@etu.hesge.ch>
2021-12-25 15:09:56 +0100

    Added blockchain

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import block
 import hashlib
+from block import block
 from transaction import transaction
+from blockchain import blockchain
 
 if __name__ == "__main__":
 
@@ -9,11 +10,16 @@ if __name__ == "__main__":
 	t.append(transaction("Jonas" , "Graham"  , 112))
 	t.append(transaction("Graham" , "Joseph" , 115))
 	t.append(transaction("Jack"   , "Patrick", 100))
-	t.append(transaction("Patrick", "Steph"  , 42))
-	t.append(transaction("Steph"  , "Graham" , 8))
-	t.append(transaction("Marlene", "Jack"   , 1))
+	
+	t2 = []
+	t2.append(transaction("Patrick", "Steph"  , 42))
+	t2.append(transaction("Steph"  , "Graham" , 8))
+	t2.append(transaction("Marlene", "Jack"   , 1))
 
-	# Create a new block
-	block_1 = block.block(1, t, hashlib.sha256("0".encode('ASCII')).hexdigest())
-	block_1.show_hashed()
-	block_1.show()
+	#block(0, t, "0").show_hashed()
+
+	graham_coin = blockchain()
+	graham_coin.add_block(t)
+	graham_coin.add_block(t2)
+	
+	graham_coin.show_hashed()
